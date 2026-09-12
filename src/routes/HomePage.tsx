47077e34e178d4aadcee85ca7fdd98b4e573e422
jessicaagren <jessicaagren@hotmail.com>
2026-09-12 18:49:25 +0200

    Refine HomePage introduction for clarity and detail

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -4,8 +4,9 @@ export default function HomePage() {
       <div className='HomePage'>
         Hi! I'm Jessica, a passionate frontend developer with previous
         experience in marketing and communications. I have a degree in frontend
-        development and have developed several fullstack projects. Iam right now
-        learning more about system development.
+        development and have built several fullstack projects from the ground
+        up. I am right now learning more about system development and building
+        databases.
       </div>
     </>
   );
