4e95e8c36e51cfbd9b2f6a164f63940e0ca409ba
jessicaagren <jessicaagren@hotmail.com>
2026-09-12 18:39:26 +0200

    Update Footer component to display current year and author name

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,13 @@
 import './Footer.scss';
 
 export default function Footer() {
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  };
+
   return (
     <>
-      <footer className='Footer'>footer</footer>
+      <footer className='Footer'>Jessica Ågren {getCurrentYear()}</footer>
     </>
   );
 }
