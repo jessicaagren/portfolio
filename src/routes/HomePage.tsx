cdc968826832704cc5f639114cd1c45286f5693a
jessicaagren <jessicaagren@hotmail.com>
2026-09-12 18:40:01 +0200

    Update HomePage component to include personal introduction and professional background

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,7 +1,12 @@
 export default function HomePage() {
   return (
     <>
-      <div>home</div>
+      <div className='HomePage'>
+        Hi! I'm Jessica, a passionate frontend developer with previous
+        experience in marketing and communications. I have a degree in frontend
+        development and have developed several fullstack projects. Iam right now
+        learning more about system development.
+      </div>
     </>
   );
 }
