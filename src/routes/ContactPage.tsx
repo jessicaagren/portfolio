f6f132d1386e54b8efa63600b1694686a19253de
jessicaagren <jessicaagren@hotmail.com>
2026-09-12 18:40:34 +0200

    Implement ContactPage with email and social media links

--- src/routes/ContactPage.tsx
+++ src/routes/ContactPage.tsx
@@ -1,7 +1,21 @@
 export default function ContactPage() {
   return (
     <>
-      <div>contact</div>
+      <div className='ContactPage'>
+        <a href='mailto:jessicaagren@hotmail.com'>Email me</a>
+        <a
+          href='https://www.linkedin.com/in/jessicaagren/'
+          target='_blank'
+          rel='noopener noreferrer'>
+          LinkedIn
+        </a>
+        <a
+          href='https://github.com/jessicaagren'
+          target='_blank'
+          rel='noopener noreferrer'>
+          GitHub
+        </a>
+      </div>
     </>
   );
 }
