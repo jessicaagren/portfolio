64c2f75404fedc250fd46710277fb3cffbe26296
jessicaagren <jessicaagren@hotmail.com>
2026-09-15 13:00:56 +0200

    Add Roll To Do project with associated image and update project types

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,8 +3,11 @@ import astronomyImage from '../assets/Astronomy-API.png';
 import scrollOfThronesImage from '../assets/scroll-of-thrones.png';
 import theLiteraryGardenImage from '../assets/the-literary-garden.png';
 import recipeInPiecesImage from '../assets/recipe-in-pieces.png';
+import rollToDoImage from '../assets/roll-to-do.png';
+const placeholderImg =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEFKwKKIne5qyG8tU25RYExTtpXsXX9OJt1Xi9Fmtdcnf6JQsEMsSX1vQ&s=10';
 
-export type Project = {
+export type DevProject = {
   slug: string;
   name: string;
   altText: string;
@@ -13,8 +16,15 @@ export type Project = {
   technologies: string[];
   github?: string;
 };
+export type CommProject = {
+  slug: string;
+  name: string;
+  altText: string;
+  images: string[];
+  description: string;
+};
 
-export const projects: Project[] = [
+export const devProjects: DevProject[] = [
   {
     slug: 'folkuniversitetet',
     name: 'Folkuniversitetet',
@@ -24,6 +34,7 @@ export const projects: Project[] = [
       'Project under development for Folkuniversitetet. The platform handles event bookings, location specific data, and user management. I planned and was responsible for the frontend design together with a colleague who mainly handled the backend development. On top of working on an intuitive user interface and login process, I built reusable components via component libraries, developed logical solutions for modals and forms, and integrated an LLM (Gemini) into the project. I also contributed to the backend development.',
     technologies: [
       'React',
+      'React Router',
       'TypeScript',
       'Mantine UI',
       'Node.js',
@@ -32,6 +43,7 @@ export const projects: Project[] = [
       'Clerk',
       'Resend',
       'Gemini API',
+      'TripAdvisor API',
     ],
   },
   {
@@ -41,7 +53,13 @@ export const projects: Project[] = [
     images: [recipeInPiecesImage],
     description:
       'Personal recipe keeper for different types of food sorted into categories.',
-    technologies: ['React', 'Mantine UI', 'Sanity'],
+    technologies: [
+      'React',
+      'React Router',
+      'TypeScript',
+      'Mantine UI',
+      'Sanity',
+    ],
     github: 'https://github.com/jessicaagren/recipe-in-pieces',
   },
   {
@@ -84,5 +102,25 @@ export const projects: Project[] = [
       'This is an API providing information about the solar system, its planets, and their moons.',
     technologies: ['Node.js', 'Express', 'MySQL'],
     github: 'https://github.com/jessicaagren/astronomy-API',
+  },
+  {
+    slug: 'roll-to-do',
+    name: 'Roll To Do',
+    altText: 'Roll To Do',
+    images: [rollToDoImage],
+    description:
+      'First project using Tailwind. Main concept is a to-do-list where you roll a D20 and "kill" a dragon by completing the tasks, earning different points based on the chore.',
+    technologies: ['Node.js', 'Express', 'MySQL'],
+    github: 'https://github.com/jessicaagren/roll-to-do',
+  },
+];
+
+export const commProjects: CommProject[] = [
+  {
+    slug: 'test',
+    name: 'test',
+    altText: 'test',
+    images: [placeholderImg],
+    description: 'Coming.',
   },
 ];
